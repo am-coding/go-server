a608a72b71ac0d54044a38ed5a442e7f33a48ba2
am-coding <amrikdeveloper@gmail.com>
2020-08-05 18:55:08 +0530

    made req handler

--- server.go
+++ server.go
@@ -6,7 +6,27 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404, Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello")
+}
+
 func main() {
-	fmt.Println("server started at localhost:8000/n")
+
+	http.HandleFunc("/hello", helloHandler)
+
+    fmt.Printf("Starting server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
 
